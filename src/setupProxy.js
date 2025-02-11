147625cab86858163f30089fb56380276c1653b3
jan_wu <zip123z@gmail.com>
2025-02-11 15:38:46 +0800

    Resolve merge conflicts

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
-// filepath: /Users/jan_wu/Documents/Cline/floorplan2/floorplan-app/src/setupProxy.js
+// filepath: /Users/jan_wu/Documents/Cline/floorplan2/floorplan-app/src/setupProxy.jsß
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
+module.exports = function(app) {ß
   app.use(
     '/images',
     createProxyMiddleware({
