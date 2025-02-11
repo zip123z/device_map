28533b5fd6d8671620158e1e5f623bed49734776
jan_wu <zip123z@gmail.com>
2025-02-11 16:02:40 +0800

    Updates

---
+++ precache-manifest.bdd797d11cd722807c65641df20f9ba4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5b5d64aaad43eaaa2c247b3a62b957",
+    "url": "/device_map/index.html"
+  },
+  {
+    "revision": "31d62bfd0603be0bbb0c",
+    "url": "/device_map/static/css/2.cee453c7.chunk.css"
+  },
+  {
+    "revision": "47c5fb879bef083dd676",
+    "url": "/device_map/static/css/main.1e9a528a.chunk.css"
+  },
+  {
+    "revision": "31d62bfd0603be0bbb0c",
+    "url": "/device_map/static/js/2.74e06552.chunk.js"
+  },
+  {
+    "revision": "4cb447811eafcde62d61d67183d8b566",
+    "url": "/device_map/static/js/2.74e06552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47c5fb879bef083dd676",
+    "url": "/device_map/static/js/main.5be95d5b.chunk.js"
+  },
+  {
+    "revision": "3ff5c5cc6ac386a6fda1",
+    "url": "/device_map/static/js/runtime-main.02b81700.js"
+  }
+]);
